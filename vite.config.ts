8ae5e21d8e5836adc4666c3c0d5c9c4985321bc7
amirreza <amir.gfh.re@gmail.com>
2026-02-20 22:32:31 +0330

    v1.0.1 fixed

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:"/coding-lang-roadmap/",
+  base: "/coding-lang-roadmap/",
   plugins: [react(), tailwindcss(), viteSingleFile()],
   resolve: {
     alias: {
